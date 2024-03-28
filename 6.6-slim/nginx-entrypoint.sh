a72c3ebb11a66a525fc165867998736dbef5bc9f
jameswsullivan <81947235+jameswsullivan@users.noreply.github.com>
2024-03-28 14:26:49 -0500

    Reoriganize branches

--- 6.6-slim/nginx-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sed -i "s|CONTEXT_PATH|${CONTEXT_PATH}|g" /etc/nginx/conf.d/default.conf
-sed -i "s|SOLR_HOST|${SOLR_HOST}|g" /etc/nginx/conf.d/default.conf
-sed -i "s|SOLR_PORT|${SOLR_PORT}|g" /etc/nginx/conf.d/default.conf
-
-nginx -g "daemon off;"
